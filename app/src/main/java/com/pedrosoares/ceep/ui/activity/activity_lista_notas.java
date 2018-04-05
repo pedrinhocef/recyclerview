d77232bfea0826c5ae15dd07141de72557058e3a
Pedro Soares <pedrinhocef@gmail.com>
2018-04-05 19:57:54 -0300

    Adicionando classes dao,adapter, modelo, add xmls de layout e drawable xml

--- app/src/main/java/com/pedrosoares/ceep/ui/activity/activity_lista_notas.java
+++ app/src/main/java/com/pedrosoares/ceep/ui/activity/activity_lista_notas.java
@@ -2,8 +2,15 @@ package com.pedrosoares.ceep.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 import com.pedrosoares.ceep.R;
+import com.pedrosoares.ceep.dao.NotaDAO;
+import com.pedrosoares.ceep.model.Nota;
+import com.pedrosoares.ceep.ui.adapter.ListaNotasAdapter;
+
+import java.util.List;
 
 public class activity_lista_notas extends AppCompatActivity {
 
@@ -11,5 +18,14 @@ public class activity_lista_notas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+
+        ListView listaNotas = findViewById(R.id.listView);
+
+        NotaDAO dao = new NotaDAO();
+
+        dao.insere(new Nota("Ptrimeira nota", "Primeira Descrição"));
+        List<Nota> todasNotas = dao.todos();
+
+        listaNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
     }
 }
