10d69ede372d9cf4046dad268fbdc920f5fcb0c8
Pedro Soares <pedrinhocef@gmail.com>
2018-04-05 18:42:31 -0300

    first commit

---
+++ app/src/main/java/com/pedrosoares/ceep/ui/activity/activity_lista_notas.java
@@ -0,0 +1,15 @@
+package com.pedrosoares.ceep.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pedrosoares.ceep.R;
+
+public class activity_lista_notas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_notas);
+    }
+}
